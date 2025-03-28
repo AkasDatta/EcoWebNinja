a538bc980e82f6849a00069ea4fa320b88ac841d
Akas_Datta <012akasdatta@gmail.com>
2025-03-29 04:48:25 +0600

    added navbar and other

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('label[for="menu-toggle"]');
+const navbarDefault = document.querySelector('.peer-checked + div');
+
+toggleButton.addEventListener('click', () => {
+  navbarDefault.classList.toggle('hidden');
+});
